a5dddd787bfc5560fe121cad321edbb4c7aaa015
Alexoah YT <alexandra.soah@gmail.com>
2020-04-07 11:37:28 +0700

    added answer files for C# Data Types exercises

---
+++ W3School-CSExercises/CS-DataTypes/csDataTypesE1.cs
@@ -0,0 +1,18 @@
+/**
+ *	from C# Data Types: Exercise 1 ( https://www.w3schools.com/cs/exercise.asp?filename=exercise_data_types1 )
+ *
+ *	question:
+ *	Add the correct data type for the following variables:
+ *
+ *	___ myNum = 9;
+ *	______ myDoubleNum = 8.99;
+ *	____ myLetter = 'A';
+ *	____ myBoolean = false;
+ *	______ myText = "Hello World";
+ *
+ */
+int myNum = 9;
+double myDoubleNum = 8.99;
+char myLetter = 'A';
+bool myBoolean = false;
+string myText = "Hello World";
---
+++ W3School-CSExercises/CS-DataTypes/csDataTypesE4.cs
@@ -0,0 +1,12 @@
+/**
+ *	from C# Data Types: Exercise 4 ( https://www.w3schools.com/cs/exercise.asp?filename=exercise_data_types4 )
+ *
+ *	question:
+ *	Type Casting - Use the correct conversion method to convert the int variable to a string:
+ *
+ *	int myInt = 10;
+ *	Console.WriteLine(Convert.________(_____));
+ *
+ */
+int myInt = 10;
+Console.WriteLine(Convert.ToString(myInt));
