d655aa4fc881bc146391206ae2fc226ed708fab8
Alexoah YT <alexandra.soah@gmail.com>
2020-04-07 11:35:46 +0700

    added answer files for C# Syntax exercises

---
+++ W3School-CSExercises/CS-Syntax/csSyntaxE1.cs
@@ -0,0 +1,16 @@
+/**
+ *	from C# Syntax: Exercise 1 ( https://www.w3schools.com/cs/exercise.asp?filename=exercise_syntax1 )
+ *
+ *	question:
+ *	Insert the missing part of the code below to output "Hello World!".
+ *
+ *	static void ____(string[] args)
+ *	{
+ *		_______._________("Hello World!");    
+ *	}
+ *
+ */
+static void Main(string[] args)
+{
+	Console.WriteLine("Hello World!");    
+}
---
+++ W3School-CSExercises/CS-Syntax/csSyntaxE2.cs
@@ -0,0 +1,12 @@
+/**
+ *	from C# Syntax: Exercise 2 ( https://www.w3schools.com/cs/exercise.asp?filename=exercise_syntax2 )
+ *
+ *	question:
+ *	Comments in C# are written with special characters. Insert the missing parts:
+ *
+ *	__ This is a single-line comment
+ *	__ This is a multi-line comment __
+ *
+ */
+// This is a single-line comment
+/* This is a multi-line comment */
