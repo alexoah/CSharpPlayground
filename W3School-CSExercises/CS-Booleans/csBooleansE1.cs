d9fa16a45c5de91b68788473542c8f1cd2e59cf2
Alexoah YT <alexandra.soah@gmail.com>
2020-04-07 15:53:08 +0700

    added answer files for C# Booleans exercises

---
+++ W3School-CSExercises/CS-Booleans/csBooleansE1.cs
@@ -0,0 +1,16 @@
+/**
+ *	from C# Booleans: Exercise 1 ( https://www.w3schools.com/cs/exercise.asp?filename=exercise_booleans1 )
+ *
+ *	question:
+ *	Fill in the missing parts to print the values True and False:
+ *
+ *	____ isCodingFun = true;
+ *	____ isFishTasty = false;
+ *	Console.WriteLine(___________);
+ *	Console.WriteLine(___________);
+ *
+ */
+bool isCodingFun = true;
+bool isFishTasty = false;
+Console.WriteLine(isCodingFun);
+Console.WriteLine(isFishTasty);
---
+++ W3School-CSExercises/CS-Booleans/csBooleansE2.cs
@@ -0,0 +1,14 @@
+/**
+ *	from C# Booleans: Exercise 2 ( https://www.w3schools.com/cs/exercise.asp?filename=exercise_booleans2 )
+ *
+ *	question:
+ *	Fill in the missing parts to print the value True:
+ *
+ *	int x = 10;
+ *	int y = 9;
+ *	Console.WriteLine(_ _ _);
+ *
+ */
+int x = 10;
+int y = 9;
+Console.WriteLine(x > y);
